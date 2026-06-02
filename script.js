afd9e1943a170c69bcd790dd985e9f3c0337e0eb
komal2015bharti-a11y <komal2015bharti@gmail.com>
2026-06-02 22:18:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 const calendar = document.getElementById("calendar");
+
 const popup = document.getElementById("popup");
+
 const popupTitle = document.getElementById("popupTitle");
+
 const popupMessage = document.getElementById("popupMessage");
+
 const closeBtn = document.getElementById("closeBtn");
 
 fetch("./messages.json")
+
   .then(response => response.json())
+
   .then(data => {
 
     const today = new Date();
@@ -13,26 +19,32 @@ fetch("./messages.json")
     Object.keys(data).forEach(date => {
 
       const card = document.createElement("div");
+
       card.classList.add("day-card");
 
       const unlockDate = new Date(date);
 
       card.innerHTML = `
         <h3>${date}</h3>
+        <p>${today >= unlockDate ? "💌 Open" : "🔒 Locked"}</p>
       `;
 
       if (today >= unlockDate) {
 
         card.addEventListener("click", () => {
+
           popup.classList.remove("hidden");
+
           popupTitle.innerText = data[date].title;
+
           popupMessage.innerText = data[date].message;
+
         });
 
       } else {
 
         card.classList.add("locked");
-        card.innerHTML += `<p>🔒 Locked</p>`;
+
       }
 
       calendar.appendChild(card);
@@ -40,22 +52,29 @@ fetch("./messages.json")
     });
 
   })
+
   .catch(error => {
-    console.error("Error loading messages:", error);
+
+    console.error(error);
 
     calendar.innerHTML = `
-      <p style="color:white;">
-        Failed to load messages 💔
-      </p>
+      <p>Failed to load messages 💔</p>
     `;
+
   });
 
 closeBtn.addEventListener("click", () => {
+
   popup.classList.add("hidden");
+
 });
 
 window.addEventListener("click", (e) => {
+
   if (e.target === popup) {
+
     popup.classList.add("hidden");
+
   }
+
 });
